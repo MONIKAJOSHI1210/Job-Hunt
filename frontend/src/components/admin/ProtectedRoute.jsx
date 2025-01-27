84f164ca268069625400e8a213ba707fb7b87078
MONIKAJOSHI1210 <monikajoshi1210@gmail.com>
2025-01-27 09:28:10 +0530

    Commit

--- frontend/src/components/admin/ProtectedRoute.jsx
+++ frontend/src/components/admin/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
-const { useNavigate } = require("react-router-dom");
-const {useSelector}=require("react-redux");
-const { useEffect } = require("react");
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 
 const ProtectedRoute =({children})=>{
     const {user}=useSelector(store=>store.auth);
